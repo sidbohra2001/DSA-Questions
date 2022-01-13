bec4592736ba2af6d3fc86345f094e534c9622ce
Siddhant Bohra <57746434+sidbohra2001@users.noreply.github.com>
2022-01-13 13:25:53 +0530

    Create Pattern-5.cpp

---
+++ Pattern-5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for(int i = 0; i<=n; i++)
+    {
+        for(int j = n-1-i; j>=0; j--) cout<<" ";
+        for(int j = 0; j<=i; j++) cout<<"* ";
+        cout<<endl;
+    }
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<=i; j++) cout<<" ";
+        for(int j = i; j<n; j++) cout<<"* ";
+        cout<<endl;
+    }
+}
